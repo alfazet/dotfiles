1505a3498279a808ecd3255705fdffbf9036988f
azasada <zsd.antoni@gmail.com>
2023-08-06 17:21:21 +0200

    status bar improvements

--- i3/scripts/volume_level.py
+++ i3/scripts/volume_level.py
@@ -4,11 +4,16 @@ import re
 import subprocess
 
 LOGO = "󰖀"
+LOGO_MUTE = "󰝟" 
 
 def main():
-    match = re.search(r"\d+%", subprocess.getoutput("pactl get-sink-volume 3"))
-    if match:
-        print(f"{LOGO} {match.group(0)}")
+    mute = "yes" in subprocess.getoutput("pactl get-sink-mute 3")
+    if mute:
+        print(f"{LOGO_MUTE}")
+    else:
+        match = re.search(r"\d+%", subprocess.getoutput("pactl get-sink-volume 3"))
+        if match and not mute:
+            print(f"{LOGO} {match.group(0)}")
 
 if __name__ == "__main__":
     main()
