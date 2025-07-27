ddef20389606e691ff198906e20ae412ff10de56
Antoni Zasada <zsd.antoni@gmail.com>
2025-07-27 10:58:26 +0200

    2025-07-27 re-theming v3

--- nvim/lua/core/plugins/colorscheme.lua
+++ nvim/lua/core/plugins/colorscheme.lua
@@ -1,41 +1,35 @@
-return {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    priority = 1000,
-    config = function()
-        require("rose-pine").setup({
-            styles = {
-                transparency = true,
-            },
-            highlight_groups = {
-                MsgArea = { bg = "none" },
-                StatusLine = { bg = "none" },
-                StatusLineNC = { bg = "none" },
-                CurSearch = { fg = "text", bg = "subtle", inherit = false },
-                QuickFixLine = { fg = "rose" },
-                CmpNormal = { bg = "base" },
-                FzfLuaPathLineNr = { fg = "foam" },
-                FzfLuaBufNr = { fg = "rose" },
-            },
-        })
-        vim.cmd.colorscheme("rose-pine")
-    end,
-}
-
 -- return {
---     dir = "/home/antek/.local/share/nvim/lazy/one-shot-dark",
+--     "rose-pine/neovim",
+--     name = "rose-pine",
+--     priority = 1000,
 --     config = function()
---         local theme = require("one-shot-dark/init")
---         theme.setup({
---             transparent = true,
---             italics = {
---                 comments = false,
---                 keywords = true,
---                 functions = true,
---                 strings = false,
---                 variables = true,
+--         require("rose-pine").setup({
+--             styles = {
+--                 transparency = true,
+--             },
+--             highlight_groups = {
+--                 MsgArea = { bg = "none" },
+--                 StatusLine = { bg = "none" },
+--                 StatusLineNC = { bg = "none" },
+--                 CurSearch = { fg = "text", bg = "subtle", inherit = false },
+--                 QuickFixLine = { fg = "rose" },
+--                 CmpNormal = { bg = "base" },
+--                 FzfLuaPathLineNr = { fg = "foam" },
+--                 FzfLuaBufNr = { fg = "rose" },
 --             },
 --         })
---         theme.colorscheme()
+--         vim.cmd.colorscheme("rose-pine")
 --     end,
 -- }
+
+return {
+    dir = "/home/antek/.local/share/nvim/lazy/red-phosphor",
+    config = function()
+        local theme = require("red-phosphor/init")
+        theme.setup({
+            transparent = true,
+            integrations = { "fzf", "cmp", "ibl" },
+        })
+        theme.colorscheme()
+    end,
+}
