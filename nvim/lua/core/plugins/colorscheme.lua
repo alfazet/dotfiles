bc882892d9ff12383267490ef93352133021f1f5
Antoni Zasada <zsd.antoni@gmail.com>
2026-02-03 20:23:34 +0100

    2026-02-03 hyprland desktop

--- nvim/lua/core/plugins/colorscheme.lua
+++ nvim/lua/core/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 return {
-    url = "https://github.com/alfazet/red-phosphor.nvim",
+    "alfazet/red-phosphor.nvim",
     config = function()
         local theme = require("red-phosphor/init")
         theme.setup({
